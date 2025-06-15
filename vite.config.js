3d4781fffa0b694312d6fa544a6ff8deb9004dd2
Ramyashri Padmanabhakumar <57267583+rum1887@users.noreply.github.com>
2025-06-15 15:16:22 +0530

    (bug): Blocked request. This host ("wiki-frontend-4ab7.onrender.com") is not allowed. To allow this host, add "wiki-frontend-4ab7.onrender.com" to `preview.allowedHosts` in vite.config.js.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: true,
+    port: process.env.PORT || 5173,
+    allowedHosts: [
+      'localhost',
+      'wiki-tagging-frontend.onrender.com',
+      'wiki-frontend-4ab7.onrender.com'
+    ]
+  },
+  server: {
+    host: true,
+    port: 5173
+  }
 })
